7a32a87f07085b16e9e0b8c320c4d5d14203b644
SUKANTA BISWAS <129424328+sukantabiswas27@users.noreply.github.com>
2025-09-11 12:48:41 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,2 +1,4 @@
+docker stop my-portfolio
+docker rm my-portfolio
 docker build -t my-portfolio .
-docker run -d -p 3027:80 my-portfolio:latest
+docker run -d -p 3027:80 --name my-portfolio my-portfolio:latest
